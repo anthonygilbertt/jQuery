d0e80af81d4f739f630ef8ac961e93b36e2cc93d
Anthony Brian Gilbert <anthonyg@uark.edu>
2018-03-01 11:50:26 -0600

    Add js and README

---
+++ learn-jquery.js
@@ -0,0 +1,30 @@
+$document).ready(funtion(){
+
+  //$('#id')
+
+  //BASIC SELECTORS
+  $('P').css('border', '4px solid red')
+  $('*').css('border', '4px solid red')
+  $('#ic').css('border', '4px solid red')
+  // BASIC ANIMATIONS
+
+  //  $('.box:first').hide(500).delay(300).show(800);
+    $('.box:first').animate({height: '200PX'}, 300)
+
+  // INDEX FILTERS
+  $('p:lt(2)').ANIMATE({HEIGHT: '200PX'}, 300)
+
+  //RELATIONSHIP FILTERS
+
+
+  //ATTRIBUTE FILTERS
+
+
+});
+
+
+$(function(){
+
+  //da codes
+
+})
